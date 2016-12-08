996d2953c80f9d50dad7e69ea2c68a2c57b3321c
shsa5563 <shsa5563@colorado.edu>
2016-12-08 14:23:15 -0700

    appending the changes in capsense header file

--- capsense.h
+++ capsense.h
@@ -1,24 +1,23 @@
 /*
  * capsense.h
  *
- *  Created on: 08-Dec-2016
+ * Created on: 02-Dec-2016
  *      Author: Satyanarayana
  */
 
 #ifndef SOURCES_CAPSENSE_H_
 #define SOURCES_CAPSENSE_H_
+#include "stdint.h"
+#include "MKL25Z4.h"
+#define NCHANNELS 16
+void capsense_init(uint32_t channel_mask);
+int touch_data(int channel);
+void scan_start(int channel);
+uint16_t scan_data(void);
 
-int touch(int channel);
-inline static void scan_strt(int channel);
-inline static uint16_t scan_data(void);
-void tsi_init(uint32_t channel_mask);
-void TSI0_IRQHandler() __attribute__((interrupt("IRQ")));
-void TSI0_IRQHandler(void);
-void tsi_data();
 
-void sliderRead(void);
-    void selfCalibration(void);
 
-    float readPercentage();
-
+ uint16_t raw_counts[NCHANNELS];
+uint16_t base_counts[NCHANNELS];
+ uint32_t enable_mask;
 #endif /* SOURCES_CAPSENSE_H_ */
