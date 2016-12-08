d3ece20d51773d1b1d22b9b1d172ba241f3e24ec
shsa5563 <shsa5563@colorado.edu>
2016-12-08 14:41:45 -0700

    Adding Unit Test for Capasitive Sense

---
+++ unit_test.c
@@ -0,0 +1,40 @@
+/*
+ * unit_test.c
+ *
+ *  Created on: 02-Dec-2016
+ *      Author: Satyanarayana
+ */
+
+
+#include "capsense.h"
+#include <stdio.h>
+#include "main.h"
+uint8_t test_status;
+
+uint8_t  test_touch()
+{
+	int output = capsense_init(10);
+	if (!(output > 0))
+	{
+		return test_status =1 ;
+	}
+	else
+	{
+		return test_status = 0;
+	}
+}
+
+
+uint8_t  test_not_touch()
+{
+	int output = capsense_init(10);
+	if (output > 0)
+	{
+		return test_status =1 ;
+	}
+	else
+	{
+		return test_status = 0;
+	}
+}
+
