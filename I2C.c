23d9754a5b65ba70c519802a98914f0f0de5173a
SayanBar <saba7535@colorado.edu>
2016-12-08 10:32:18 -0700

    I2C read and write function

---
+++ I2C.c
@@ -0,0 +1,104 @@
+#include "MKL25Z4.h"
+#include<stdio.h>
+#include<stdint.h>
+#include "I2C.h"
+
+void I2C_WriteRegister(unsigned char u8SlaveAddress, unsigned char u8RegisterAddress, /*unsigned*/ char u8Data)
+{
+	I2C_Start();	          
+	I2C0_D = u8SlaveAddress << 1;									/* Send I2C device address with W/R bit = 0 */
+	while((I2C0_S & I2C_S_TCF_MASK)==0);
+	while(!(I2C0_S & I2C_S_RXAK_MASK)==0);
+	I2C_Wait();
+
+	I2C0_D = u8RegisterAddress;										/* Send register address */
+	while((I2C0_S & I2C_S_TCF_MASK)==0);
+	while(!(I2C0_S & I2C_S_RXAK_MASK)==0);
+	I2C_Wait();
+
+	I2C0_D = u8Data;												/* Send the data */
+	while((I2C0_S & I2C_S_TCF_MASK)==0);
+	while(!(I2C0_S & I2C_S_RXAK_MASK)==0);
+	I2C_Wait();
+
+	I2C_Stop();
+
+    Pause(50);
+}
+
+unsigned char I2C_ReadRegister(unsigned char u8SlaveAddress, unsigned char u8RegisterAddress)
+{
+	unsigned char result;
+	  
+	I2C_Start();	          
+	I2C0_D = u8SlaveAddress << 1;									/* Send I2C device address with W/R bit = 0 */
+	while((I2C0_S & I2C_S_TCF_MASK)==0);
+	I2C_Wait();										
+
+	I2C0_D = u8RegisterAddress;										/* Send register address */
+	while((I2C0_S & I2C_S_TCF_MASK)==0);
+	I2C_Wait();
+
+	I2C_RepeatedStart();
+	
+	I2C0_D = (u8SlaveAddress << 1) | 0x01;							/* Send I2C device address this time with W/R bit = 1 */ 
+	while((I2C0_S & I2C_S_TCF_MASK)==0);
+	I2C_Wait();	
+
+	I2C_EnterRxMode();
+	I2C_DisableAck();
+
+	result = I2C0_D;												
+	I2C_Wait();
+	I2C_Stop();  
+	result = I2C0_D;
+	Pause(50);
+	return result;
+}
+
+void I2C_ReadMultiRegisters(unsigned char u8SlaveAddress, unsigned char u8RegisterAddress, unsigned char n, unsigned char *r)
+{
+	char i;
+	
+	I2C_Start();	          
+	I2C0_D = u8SlaveAddress << 1;									/* Send I2C device address with W/R bit = 0 */
+	I2C_Wait();										
+
+	I2C0_D = u8RegisterAddress;										/* Send register address */
+	I2C_Wait();
+
+	I2C_RepeatedStart();
+		
+	I2C0_D = (u8SlaveAddress << 1) | 0x01;							/* Send I2C device address this time with W/R bit = 1 */ 
+	I2C_Wait();	
+
+	I2C_EnterRxMode();
+	I2C_EnableAck();
+	
+	i = I2C0_D;
+	I2C_Wait();
+	
+	for(i=0; i<n-2; i++) 
+	{
+	    *r = I2C0_D;
+	    r++;
+	    I2C_Wait();
+	}
+	
+	I2C_DisableAck();
+	*r = I2C0_D;
+	r++;
+	I2C_Wait();
+	I2C_Stop();
+	*r = I2C0_D; 
+	Pause(50);		
+}
+
+void Pause(int number)
+{
+	int cnt;
+	for(cnt=0; cnt<number; cnt++)
+	{
+		//asm("nop");
+	};
+}
