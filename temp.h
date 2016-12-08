0263c1613ec2f73889c9fcae799fc4aa4200ae58
SayanBar <saba7535@colorado.edu>
2016-12-08 10:35:30 -0700

    Header file for temp.c

---
+++ temp.h
@@ -0,0 +1,24 @@
+/*
+ * zero.h
+ *
+ *  Created on: Dec 6, 2016
+ *      Author: shva9978
+ */
+
+#ifndef SOURCES_TEMP_H_
+#define SOURCES_TEMP_H_
+
+void reverse(char *str, int len);
+void send_data(char character);
+void print_string(char *ptr_str, uint32_t len);
+void print_stringl(char *ptr);
+int intToStr(int x, char str[], int d);
+int power(int a, int n);
+void ftoa(float n, char *res, int afterpoint);
+uint16_t adc_temp(void);
+void UART0_init(void);
+void Calc_temp();
+float Conv_temp (float b);
+
+
+#endif /* SOURCES_TEMP_H_ */
